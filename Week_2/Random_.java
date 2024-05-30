94aeb0e0c109a78abfdea48044c1676e5aa215de
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-05-30 14:25:37 +0600

    Add files via upload

---
+++ Week_2/Random_.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		int x = random.nextInt();
+		double y = random.nextDouble();
+		
+		
+		System.out.println(y);
+		System.out.println(x);
+				
+				
+	}
+
+}
