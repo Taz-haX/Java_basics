9eba1685d45516debdae09f25f8562eb77224214
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 16:05:33 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,30 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(add(3.66,4.4,8.3,6.55));
+		
+	
+	}
+	
+	
+	static int add(int x, int y) {
+		System.out.println("Method 1");
+		return x+y;
+	}
+	static int add(int x, int y, int z) {
+		System.out.println("Method 2");
+		return x+y+z;
+	}
+	static double add(double x, double y, double z, double p) {
+		System.out.println("Method 3");
+		return x+y+z+p;
+	}
+	
+
+}
