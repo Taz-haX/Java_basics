9e9da5fc8e839497b8030830acd18a8799a498f0
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 19:42:54 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,20 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		DiceRoller dice = new DiceRoller();
+		
+		System.out.println("Oaa");
+		
+		dice.roll();
+		
+		
+		
+	}
+	
+}
