c9e670f6d7222d27e0abe80a01b18e09afc277eb
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 17:07:03 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,31 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		int cost = -1000;
+		double costs = 10000.05;
+		
+		System.out.printf("THis thing costs us about %,.4f",costs);
+		
+		System.out.println("\n---------------------------------------------");
+		
+		System.out.printf("THis thing costs us about %+010d",cost);
+		
+		System.out.println("\n---------------------------------------------");
+		
+		String ss = "Narnia";
+		System.out.printf("You really wanna go to %s", ss);
+		
+		System.out.println("\n---------------------------------------------");
+		
+		boolean fb = true;
+		System.out.printf("%b",fb);
+			
+	}
+	
+}
