ea0f4bb149be85dfce412878664e5ef6ca6cc333
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 18:34:49 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,26 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		cars car_1 = new cars();
+		cars car_2 = new cars();
+		cars car_3 = new cars();
+		cars car_4 = new cars();
+		
+		System.out.println(car_1.model);
+		car_1.stop();
+		
+		System.out.println(car_3.make);
+		car_4.drive();
+		
+		System.out.println(car_2.price);
+		
+		
+	}
+	
+}
