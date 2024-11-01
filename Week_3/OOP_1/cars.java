236abd5d18649eff9366eae8e74947b0b02d1668
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 18:36:28 +0600

    Add files via upload

---
+++ Week_3/OOP_1/cars.java
@@ -0,0 +1,18 @@
+
+public class cars {
+	
+	String make = "Porsche";
+	String model = "911";
+	int year = 2012;
+	String colour = "White";
+	double price = 259999.99;
+	
+	void drive() {
+		System.out.println("Just pushed the breaks");
+	}
+	
+	void stop() {
+		System.out.println("Just hit the pedal");
+	}
+	
+}
