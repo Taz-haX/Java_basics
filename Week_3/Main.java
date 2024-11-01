bc699f78d22d1cce0acf12337538e878f7b178c7
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 12:43:35 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,34 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name = "IDIOT";
+		
+		int age = 17;
+		
+		hello(name, age);
+		
+		System.out.println("--------------------------------------------");
+		
+		int a = 23;
+		int b = 16;
+		
+		System.out.println(add(a,b));
+		
+	}
+	
+	static void hello(String args, int arg) {
+		
+		System.out.println("DO NOT DISTURB ME YOU "+arg+" years old "+args+"!!!");
+	}
+	
+	static int add(int x, int y) {
+		int z = x+y;
+		return z;
+	}
+
+}
