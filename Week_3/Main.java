0afa68f4494ec28f1c04a1895452f389590c5a93
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 16:28:00 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,35 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {	
+	
+		int x = 90;
+		
+		String b = String.valueOf(x);
+		System.out.println(x);
+		
+		int y = Integer.parseInt(b);
+		System.out.println(y);
+		
+		double z = Double.parseDouble(b);
+		System.out.println(z);
+		
+		int p = (int)z;
+		System.out.println(p);
+		
+		double q = p;
+		System.out.println(q);
+		
+		float f = x;
+		System.out.println(f);
+		
+		int j = (int)f;
+		System.out.println(j);
+		
+	}
+	
+}
