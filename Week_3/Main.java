f2a686e1fc28f2bb70f68ef525220f147a0f5eb4
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-01 19:19:04 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,21 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		human hman = new human("Gari",29,76.2);	
+		human humn = new human("Pudri",22,110.5);
+		
+		System.out.println(hman.age);
+		System.out.println(humn.weight);
+		
+		humn.run();
+		hman.sleep();
+		
+	}
+	
+}
