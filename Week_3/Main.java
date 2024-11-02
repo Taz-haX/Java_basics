aadc608e6014c6934edb9a29a0d382bf90766d39
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-02 13:53:43 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,36 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		pizza piz = new pizza();
+		
+		pizza piz2 = new pizza("Pineapples");
+		pizza piz3 = new pizza("juiceeee","mama miaaa","mozarella","NAGAAA");
+		
+		
+		System.out.println(piz2.name);
+		System.out.println(piz2.topping);
+		System.out.println(piz2.Spice_levels);
+		
+		System.out.println("----------------------");
+		
+		System.out.println(piz.Spice_levels);
+		System.out.println(piz.Da_Sauceee);
+		
+		System.out.println("-------------------------");
+		
+		System.out.println(piz3.Da_Sauceee);
+		System.out.println(piz3.topping);
+		System.out.println(piz3.name);
+		System.out.println(piz3.Spice_levels);
+		
+		
+	}
+	
+}
