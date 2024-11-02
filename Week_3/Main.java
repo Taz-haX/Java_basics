593a8d2e7ad0d68101dfc41eb33b398df14d4ab7
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-11-02 14:38:12 +0600

    Add files via upload

---
+++ Week_3/Main.java
@@ -0,0 +1,16 @@
+//import javax.swing.JOptionPane;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		pinik pink = new pinik();
+		
+		System.out.println(pink);
+		
+	}
+	
+}
